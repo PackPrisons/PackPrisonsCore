2beb2a4a2033d73fcaf745a5479aa54488f2c9c2
Eric <65141753+Indeediouss@users.noreply.github.com>
2020-11-18 23:30:21 -0500

    Added Files

---
+++ PackPrisonsCore/src/main/java/com/packprisons/core/PackPrisonsCore.java
@@ -0,0 +1,20 @@
+package com.packprisons.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PackPrisonsCore extends JavaPlugin {
+
+    /**
+     * This is class initializes and registers all the classes/commands/events
+     */
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
