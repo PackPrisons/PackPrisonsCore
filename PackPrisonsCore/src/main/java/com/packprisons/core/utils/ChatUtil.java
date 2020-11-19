f3c5b54cd17a5d1ccc45c50a0e076010ba3d0a7c
Eric <65141753+Indeediouss@users.noreply.github.com>
2020-11-18 23:50:09 -0500

    Created ChatUtils
    Translates color, bold, italic, etc, instead of having to use ChatColor.COLOR(Message) all the time.

---
+++ PackPrisonsCore/src/main/java/com/packprisons/core/utils/ChatUtil.java
@@ -0,0 +1,19 @@
+package com.packprisons.core.utils;
+
+import org.bukkit.ChatColor;
+
+/**
+ * This class is responsible for translating color, bold, italic, etc.
+ *
+ * - Indeedious
+ */
+public class ChatUtil {
+
+    public ChatUtil(String text) {
+        translate(text);
+    }
+
+    public String translate(String str) {
+        return ChatColor.translateAlternateColorCodes('&', str);
+    }
+}
