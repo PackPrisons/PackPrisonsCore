25dddebc21fffe6a200cd54ed3df90955f068e03
Eric <65141753+Indeediouss@users.noreply.github.com>
2020-11-19 19:15:26 -0500

    Added the main funtion to this Command
    Added a simple way to send messages for now; this command will be re-written when the our upcoming API is ready.

--- PackPrisonsCore/src/main/java/com/packprisons/core/private_message/MessageCommand.java
+++ PackPrisonsCore/src/main/java/com/packprisons/core/private_message/MessageCommand.java
@@ -1,5 +1,8 @@
 package com.packprisons.core.private_message;
 
+import com.packprisons.core.PackPrisonsCore;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,11 +10,44 @@ import org.bukkit.entity.Player;
 
 public class MessageCommand implements CommandExecutor {
 
+    private final PackPrisonsCore plugin;
+
+    public MessageCommand(PackPrisonsCore plugin) {
+        this.plugin = plugin;
+    }
+
+    //@TODO: Finish this command, make it configurable, use the upcoming API when sending messages - Indeedious
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
-            Player player = (Player) sender;
 
+            Player player = (Player) sender;
+            Player target = Bukkit.getPlayerExact(args[0]);
+
+            if (args.length >= 2) {
+                if (target != null) {
+                    if (args[0].equalsIgnoreCase(target.getName())) {
+
+                        StringBuilder message = new StringBuilder();
+
+                        for (int i = 0; i < args.length; i++) {
+                            message.append(args[i]).append(" ");
+                        }
+
+                        player.sendMessage(ChatColor.GREEN + "to " + target.getName() + ChatColor.GRAY + message.toString());
+                        target.sendMessage(ChatColor.GREEN + "from " + player.getName() + ChatColor.GRAY + message.toString());
+
+                        plugin.getMessageManager().recentlyMessaged.put(player, target);
+                    }
+                } else {
+                    player.sendMessage(ChatColor.RED + "Player cannot be found!");
+                    return true;
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "Invalid Usage! /msg <player> <message>");
+                return true;
+            }
             //@TODO: Finish this Message Command, and allow the players rank to be seen in messages once the Rank plugin is created
         } else {
             return true;
