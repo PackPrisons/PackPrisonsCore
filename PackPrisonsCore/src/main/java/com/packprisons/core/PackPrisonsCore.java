b131628608e597f5ff27252400f4192dda0022a8
mattwithabat <61851223+mattwithabat@users.noreply.github.com>
2020-11-19 20:24:32 -0500

    Added fly command.
    - Added load config method in main class
    - Created registerCommands()
    - Created FlyCommand class

--- PackPrisonsCore/src/main/java/com/packprisons/core/PackPrisonsCore.java
+++ PackPrisonsCore/src/main/java/com/packprisons/core/PackPrisonsCore.java
@@ -1,11 +1,12 @@
 package com.packprisons.core;
 
+import com.packprisons.core.commands.FlyCommand;
 import com.packprisons.core.manager.MessageManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * This is class initializes and registers all the classes/commands/events
- *
+ * <p>
  * This plugin will be responsible for Gangs, Cartels, Mafia's, Drugs, crafting store, money, custom items, etc, all of the key features of this server
  */
 public final class PackPrisonsCore extends JavaPlugin {
@@ -14,13 +15,27 @@ public final class PackPrisonsCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        loadConfig();
+        registerCommands();
     }
 
-    public MessageManager getMessageManager() { return messageManager; }
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
 
     @Override
     public void onDisable() {
 
     }
+
+    public void loadConfig() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        ;
+    }
+
+    public void registerCommands() {
+        getCommand("fly").setExecutor(new FlyCommand());
+    }
+
 }
--- PackPrisonsCore/src/main/java/com/packprisons/core/utils/ChatUtil.java
+++ PackPrisonsCore/src/main/java/com/packprisons/core/utils/ChatUtil.java
@@ -13,7 +13,7 @@ public class ChatUtil {
         translate(text);
     }
 
-    public String translate(String str) {
+    public static String translate(String str) {
         return ChatColor.translateAlternateColorCodes('&', str);
     }
 }
